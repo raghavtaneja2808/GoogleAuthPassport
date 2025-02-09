2f7d116fe499b69386c0e80355bc179bea3bf6af
Raghav Taneja <raghavtaneja2808@gmail.com>
2025-02-09 16:15:42 +0530

    Updated authentication system

---
+++ services/passport.js
@@ -0,0 +1,12 @@
+const passport=require("passport");
+const GoogleStrategy=require('passport-google-oauth20').Strategy
+const keys=require("../config/keys.js");
+passport.use(new GoogleStrategy({
+    clientID:keys.googleClinetId,
+    clientSecret:keys.googleClientSecret,
+    callbackURL:'http://localhost:5000/auth/google/callback'
+},(accessToken,refreshToken,profile,done)=>{
+    console.log("access token",accessToken);
+    console.log("refresh token",refreshToken);
+    console.log("profile",profile);
+}))
